4e0cf1a365f8179566e078cd90c707c90ca2164f
r2003 <_r2003@mail.ru>
2016-04-28 20:40:59 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 using System;
+using System.Diagnostics;
+using System.Threading;
+
 
 public class program
 {
     public static void Main()
     {
         Console.WriteLine("Test");
-        for(var i=0;i<10000000;i++)
+        for(var i=0;i<1000;i++)
         {
             Console.WriteLine(i);
-            
-            
-            
-            
-            
-            
         }
+        var a=new Stopwatch();
+       a.Start();
+       Thread.Sleep(765);
+       a.Stop();
+       Console.WriteLine(a.Elapsed.TotalMilliseconds);
+        
+        
         Console.ReadLine();
     }  
 }
